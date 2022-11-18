6ca375975a3dc7392e2494ebd0b555c462e8d65a
Ravi Kant <iRvhny@gmail.com>
2022-11-18 18:14:35 +0530

    Projects templates and delete template added

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('projects/', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
+    # CRUD
+    path('create-project/', views.createProject, name="create-project"),
+    path('update-project/', views.updateProject, name="update-project"),
+    path('delete-project/', views.deleteProject, name="delete-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-
 
 def projects(request):
-    # return HttpResponse('This is the projects page!!')
     return render(request, 'projects/projects.html')
 
 
 def project(request, pk):
-    # return HttpResponse("This is project" + " " + str(pk))
     return render(request, 'projects/single-project.html')
+
+
+def createProject(request):
+    return render(request, "projects/project-form.html")
+
+
+def updateProject(request):
+    return render(request, "projects/project-form.html")
+
+
+def deleteProject(request):
+    return render(request, 'delete-template.html')
