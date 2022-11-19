81e1c18e987e5aa1c85cc502de9827f4357986c9
Ravi Kant <iRvhny@gmail.com>
2022-11-20 02:14:14 +0530

    Query the list of the models from the projects/models

--- projects/views.py
+++ projects/views.py
@@ -1,34 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'E-commerce Website',
-        'description': 'Fully functional e-commerce website'
-    },
-    {
-        'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'A personal website to write articles and display work'
-    },
-    {
-        'id': '3',
-        'title': 'Social Network',
-        'description': 'An open source project built by the community'
-    }
-]
+from .models import Project
 
 
 def projects(request):
-    context = {'projects': projectsList}
+    projects = Project.objects.all()
+    context = {'projects': projects}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
     projectObj = None
-    for i in projectsList:
+    for i in projects:
         if i['id'] == pk:
             projectObj = i
     context = {'project': projectObj}
