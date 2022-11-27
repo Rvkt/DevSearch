303390e2ebe7b9a84810dd5de1d89c15a4f20cfd
Ravi Kant <iRvhny@gmail.com>
2022-11-27 18:32:47 +0530

    Added users app

---
+++ users/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.profiles, name="profiles"),
+    # path('project/<str:pk>/', views.project, name="project"),
+    # CRUD
+    # path('create-project/', views.createProject, name="create-project"),
+    # path('update-project/<str:pk>/', views.updateProject, name="update-project"),
+    # path('delete-project/<str:pk>/', views.deleteProject, name="delete-project"),
+]
